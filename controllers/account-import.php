36a8ba37a02d32d93da548441ae96602beac18b2
Sam Horn <samuel.horn1@gmail.com>
2014-12-20 02:51:12 +1000

    Added instructions for the account import

--- controllers/account-import.php
+++ controllers/account-import.php
@@ -1,12 +1,28 @@
 <?php
 use Ubersite\Message;
+use Ubersite\Utils;
+
+$fileUploadErrors = [
+  0 => 'The file was uploaded successfully.',
+  1 => 'The uploaded file exceeds the upload_max_filesize directive in php.ini.',
+  2 => 'The uploaded file exceeds the MAX_FILE_SIZE directive that was specified in the HTML form.',
+  3 => 'The uploaded file was only partially uploaded.',
+  4 => 'No file was uploaded.',
+  6 => 'Missing a temporary folder.',
+  7 => 'Failed to write file to disk.',
+  8 => 'A PHP extension stopped the file upload.'
+];
 
 if (count($people) === 0) {
   $twig->addGlobal('noUsers', true);
 }
 
-if (count($people) === 0) {
-  $messages->addMessage(new Message('warning', 'There are currently no users in the database.'));
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+  if ($_FILES['file']['error']) {
+    $messages->addMessage(new Message('error', 'File upload error: '
+      . $fileUploadErrors[$_FILES['file']['error']]));
+  } else {
+    $messages->addMessage(new Message('success', 'nothing happened'));
+    Utils::refresh();
+  }
 }
-
-
