3effe6b83e9d6834a86d07b85cfe7713418ded56
Sam Horn <samuel.horn1@gmail.com>
2015-01-24 21:36:43 +1000

    Use the Group/Page/Question objects when constructing the Questionnaire object

--- src/Questionnaire.php
+++ src/Questionnaire.php
@@ -1,6 +1,10 @@
 <?php
 namespace Ubersite;
 
+use Ubersite\Questionnaire\Group;
+use Ubersite\Questionnaire\Page;
+use Ubersite\Questionnaire\Question;
+
 class Questionnaire
 {
     public $id;
@@ -15,9 +19,21 @@ class Questionnaire
         $this->id = $row['Id'];
         $this->title = $row['Name'];
 
-        $data = json_decode($row['Pages'], true);
-        $this->pages = $data['Pages'];
-        $this->groups = $data['Groups'];
-        $this->questions = $data['Questions'];
+        $data = json_decode($row['Pages']);
+
+        foreach ($data->Questions as $questionID => $question) {
+            $question->QuestionID = $questionID;
+            $this->questions[$questionID] = new Question($question);
+        }
+
+        foreach ($data->Groups as $groupID => $group) {
+            $group->GroupID = $groupID;
+            $this->groups[$groupID] = new Group($group, $this->questions);
+        }
+
+        foreach ($data->Pages as $pageID => $page) {
+            $page->PageID = $pageID;
+            $this->pages[$pageID] = new Page($page, $this->questions, $this->groups);
+        }
     }
 }
