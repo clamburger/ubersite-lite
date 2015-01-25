8d33b6df0e16b5188f1ab2ce953155ba3250af15
Sam Horn <samuel.horn1@gmail.com>
2015-01-25 14:09:46 +1000

    Now that we have an interface for it, don't create a questionnaire by default

--- setup/database.sql
+++ setup/database.sql
@@ -13,21 +13,6 @@ CREATE TABLE questionnaires (
   Intro TEXT
 );
 
-INSERT INTO `questionnaires` (`Id`, `Name`, `Pages`, `Intro`) VALUES
-  (1, 'Camp Questionnaire', '{
-    "Questions": {},
-    "Groups": {},
-    "Pages": []
-}', 'At the end of each Übertweak we get all campers to fill out a questionnaire about camp.
-
-Your feedback is extremely useful and all of the leaders spend time after camp reviewing it to ensure that the next Übertweak is better than ever!
-
-We encourage you to be completely honest: if you had a terrible time and hated all the leaders, make sure you tell us that. We will not hold any of this feedback against you.
-
-The questionnaire is broken up into a number of sections. Take as much time as you need for each section and please be completely honest! Keep in mind that once you complete a session you <strong>cannot go back to that section</strong>.
-
-Your specimen has been processed and we are now ready to begin the test proper.');
-
 CREATE TABLE users (
   Username TEXT NOT NULL PRIMARY KEY,
   Name TEXT NOT NULL,
