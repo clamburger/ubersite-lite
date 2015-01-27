8db41ed90850a2a914fad9fef32eb8e5b913a996
Sam Horn <samuel.horn1@gmail.com>
2015-01-27 23:50:47 +1000

    Removed ability to edit users the old way

--- controllers/accounts.php
+++ controllers/accounts.php
@@ -11,68 +11,33 @@ $twig->addGlobal('submit', "Create User");
 
 $twig->addGlobal('roles', User::$validRoles);
 
-if ($_SERVER['REQUEST_METHOD'] === 'POST' && $user->isLeader()) {
-    if ($_POST['action'] == "new") {
-        // Creating a new user
-        $username = $_POST['username'];
-        $name = $_POST['name'];
-        $role = $_POST['role'];
-        if ($username === '') {
-            $messages->addMessage(new Message("error", "A username must be provided."));
-        } elseif (isset($people[$username])) {
-            $messages->addMessage(new Message("error", "The provided username already exists."));
-        } elseif ($name === '') {
-            $messages->addMessage(new Message("error", "The name cannot be blank."));
-        } elseif ($role !== '' && !in_array($role, User::$validRoles)) {
-            $messages->addMessage(new Message("error", "Invalid role provided."));
-        } else {
-          // The default password is the same as the username.
-            $password = password_hash($username, PASSWORD_DEFAULT);
-            $query = 'INSERT INTO users (Username, Name, Password, Role, DutyTeam)
-                VALUES(?, ?, ?, ?, ?)';
-            $stmt = $dbh->prepare($query);
-            $stmt->execute([$username, $name, $password, $role, $_POST['dutyTeam']]);
-            $messages->addMessage(new Message('success', 'Account successfully created!'));
-
-            Utils::refresh();
-        }
-
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    // Creating a new user
+    $username = $_POST['username'];
+    $name = $_POST['name'];
+    $role = $_POST['role'];
+    if ($username === '') {
+        $messages->addMessage(new Message("error", "A username must be provided."));
+    } elseif (isset($people[$username])) {
+        $messages->addMessage(new Message("error", "The provided username already exists."));
+    } elseif ($name === '') {
+        $messages->addMessage(new Message("error", "The name cannot be blank."));
+    } elseif ($role !== '' && !in_array($role, User::$validRoles)) {
+        $messages->addMessage(new Message("error", "Invalid role provided."));
     } else {
-        // Editing an account
-        $username = $_POST['username'];
-        $name = $_POST['name'];
-        $role = $_POST['role'];
-        if (!isset($people[$username])) {
-            $messages->addMessage(new Message("error", "That user no longer exists."));
-            Utils::refresh();
-        } elseif ($name === '') {
-            $messages->addMessage(new Message("error", "The name cannot be blank."));
-        } elseif ($role !== '' && !in_array($role, User::$validRoles)) {
-            $messages->addMessage(new Message("error", "Invalid role provided."));
-        } else {
-            $query = 'UPDATE users SET Name = ?, Role = ?, DutyTeam = ? WHERE Username = ?';
-            $stmt = $dbh->prepare($query);
-            $stmt->execute([$name, $role, $_POST['dutyTeam'], $username]);
-            $messages->addMessage(new Message('success', 'Account successfully modified.'));
-            Utils::refresh();
-        }
+      // The default password is the same as the username.
+        $password = password_hash($username, PASSWORD_DEFAULT);
+        $query = 'INSERT INTO users (Username, Name, Password, Role, DutyTeam)
+            VALUES(?, ?, ?, ?, ?)';
+        $stmt = $dbh->prepare($query);
+        $stmt->execute([$username, $name, $password, $role, $_POST['dutyTeam']]);
+        $messages->addMessage(new Message('success', 'Account successfully created!'));
+
+        Utils::refresh();
     }
 }
 
-// Edit link clicked
-if ($SEGMENTS[1] == "edit") {
-    if (!isset($people[$SEGMENTS[2]])) {
-        header("Location: /accounts");
-        exit;
-    }
-    $stmt = $dbh->prepare('SELECT * FROM users WHERE Username = ?');
-    $stmt->execute([$SEGMENTS[2]]);
-    $editingUser = new User($stmt->fetch());
-    $twig->addGlobal('editing', true);
-    $twig->addGlobal('form', $editingUser);
-}
-
-// This sorts the list of people by role.
+// This sorts the list of people by username.
 usort($people, function($user1, $user2) {
     return strcmp($user1->username, $user2->username);
 });
